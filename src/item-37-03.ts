a0c888425b957bb9f7b4bd5d80e3405ffdd4bd81
Steven Lin <steven_lx@outlook.com>
2020-08-20 13:53:12 -0700

    delete item 37 file 4

--- src/item-37-03.ts
+++ src/item-37-03.ts
@@ -15,9 +15,64 @@
 
 /*
   example background
-  - you want to brand built-in types such as string or number
-  - you can define the type but it's not possible to 
-    create any values of this type.
-  - so what's the deal?
-  - you are playing a game with the type system
+  - you have a binary search function 
+  - for binary search to work the list
+    you are searching must be sorted
+    for it to work.
+  - so how can you make sure the list 
+    is sorted?
 */
+
+// well you don't know if xs is sorted
+function binarySearch<T>(xs: T[], x: T): boolean {
+  let low = 0,
+    high = xs.length - 1;
+  while (high >= low) {
+    const mid = low + Math.floor((high - low) / 2);
+    const v = xs[mid];
+    if (v === x) return true;
+    [low, high] = x > v ? [mid + 1, high] : [low, mid - 1];
+  }
+  return false;
+}
+
+/*
+  - introduce a new branded type
+  - and a helper function to help TS to 
+    infer the type
+
+*/
+
+type SortedList<T> = T[] & { _brand: "sorted" };
+
+function isSorted<T>(xs: T[]): xs is SortedList<T> {
+  for (let i = 1; i < xs.length; i++) {
+    if (xs[i] > xs[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function binarySearch2<T>(xs: SortedList<T>, x: T): boolean {
+  let low = 0,
+    high = xs.length - 1;
+  while (high >= low) {
+    const mid = low + Math.floor((high - low) / 2);
+    const v = xs[mid];
+    if (v === x) return true;
+    [low, high] = x > v ? [mid + 1, high] : [low, mid - 1];
+  }
+  return false;
+}
+
+function mySearch<T>(xs: T[], x: T): boolean {
+  if (isSorted(xs)) {
+    xs; // type is SortedList<T>
+    return binarySearch2(xs, x);
+  } else {
+    // you can't call binarySearch2
+    xs; // as the type is T[]
+    return false;
+  }
+}
